04a5908fe89f9e04ad9249c5bb75945c720e9143
barbaraspilmann <156524512+barbaraspilmann@users.noreply.github.com>
2025-03-31 13:20:18 +0100

    Added About Us page

--- constants/nav.ts
+++ constants/nav.ts
@@ -2,4 +2,5 @@ export const navItems = [
   { name: "Home", href: "/dashboard" },
   { name: "BMI Calculator", href: "/bmi-calculator" },
   { name: "Calorie Calculator", href: "/calorie-calculator" },
+  { name: "About Us", href: "/about-us" }, // New entry
 ];
