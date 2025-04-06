51a7654f53ab3dae08cb2dd408ddea6c82e53a0d
essinn <79027625+essinn@users.noreply.github.com>
2025-04-06 21:40:07 +0100

    reorder about us link in constants

--- constants/nav.ts
+++ constants/nav.ts
@@ -1,6 +1,6 @@
 export const navItems = [
   { name: "Home", href: "/dashboard" },
+  { name: "About Us", href: "/about-us" },
   { name: "BMI Calculator", href: "/bmi-calculator" },
   { name: "Calorie Calculator", href: "/calorie-calculator" },
-  { name: "About Us", href: "/about-us" }, // New entry
 ];
